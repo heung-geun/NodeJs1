8a2f1cbd880e2890bac4b159aebb493696d0250b
Hyeunggeun-Yun <gudrms121712@naver.com>
2024-11-25 15:51:27 +0900

    src 파일 위치변경

--- src/libs/run.js
+++ src/libs/run.js
@@ -1,12 +1,12 @@
 // src/libs/run.js 
 
 import math from './math.js';
-// import baseException from '../exception/base.exception.js';
-// import authenticationMiddleware from '../middleware/authentication/authentication.middleware.js';
+import baseException from '../exception/base.exception.js';
+import authenticationMiddleware from '../middleware/authentication/authentication.middleware.js';
 
 math();
-// baseException();
-// authenticationMiddleware();
+baseException();
+authenticationMiddleware();
 
 // Print: src/libs/math.js 파일입니다.
 // Print: src/exception/base.exception.js 파일입니다.
